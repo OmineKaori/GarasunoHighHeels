36c7efb625fa59190dabe37b4a9121e0fb687847
大嶺嘉織 <>
2019-11-14 07:42:11 +0900

    2019/11/14

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour {
+
+	public static float speed = 5.0f;
+	public static float slideSpeed = 2.0f;
+
+	public static Animator  animator;
+	UIScript uiscript;
+
+	// ゲームが始まった時に一回だけ呼ばれる
+	void Start () {
+		animator = GetComponent <Animator> ();
+		uiscript = GameObject.Find ("Canvas").GetComponent<UIScript> ();
+	}
+
+	void Update () {
+		transform.position += Vector3.forward * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * slideSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * slideSpeed * Time.deltaTime;
+        }
+	}
+
+	// Triggerである障害物にぶつかったとき
+	void OnTriggerEnter (Collider colider){
+
+		var stateInfo = animator.GetCurrentAnimatorStateInfo (0);
+		bool isJump = stateInfo.IsName("Base Layer.JUMP00");
+		bool isSlide = stateInfo.IsName("Base Layer.SLIDE00");
+		bool isRun = stateInfo.IsName("Base Layer.RUN00_F");
+
+		bool isHigh = colider.CompareTag("High");
+		bool isLow = colider.CompareTag("Low");
+		bool isBarrier = colider.CompareTag ("barrier");
+		bool isGoal = colider.CompareTag ("goal");
+
+
+		// 障害物に当たったとき
+		if( (isHigh == true && isSlide == false) ||
+			(isLow == true && isJump == false) ||
+		    (isBarrier == true)){
+            speed = 0;
+			animator.SetBool ("DEAD", true);
+            uiscript.Gameover();
+		}
+
+		//ゴールした時
+		if(isGoal == true){
+			speed = 0;
+
+			animator.SetBool ("WIN", true);
+			// UI
+			uiscript.Goal();
+		}
+	}
+	public void Left(){
+		transform.position += Vector3.left * PlayerScript.slideSpeed * Time.deltaTime;
+		transform.Rotate(0,-50 * Time.deltaTime,0);
+	}
+
+	public void Right(){
+		transform.position += Vector3.right * PlayerScript.slideSpeed * Time.deltaTime;
+		transform.Rotate(0,50 * Time.deltaTime,0);
+	}
+
+	public void Jump(){
+		PlayerScript.animator.SetBool ("JUMP", true);
+	}
+}
---
+++ Assets/Scripts/Recorder.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Recorder : MonoBehaviour {
+	private float time=0.0f;
+	private float[] timeNow;
+	private int[] inkey;
+	int i=0;
+	public static float Maxtime=99f;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+		//Maxtime=PlayerPrefs.GetFloat("Maxtime");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*現在進行形で行動を保存*/
+		time = canvas2Script.timer;//常に時間は動くように
+
+        //ボタンの種類と押した時の時間をそれぞれ保存
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			timeNow[i] = time;
+			inkey[i] = 1;
+			i++;
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			timeNow[i] = time;
+			inkey[i] = 0;
+			i++;
+		}
+        /*
+		if(Input.GetButtonDown("jumpbutton")){
+			timeNow[i] = time;
+			inkey[i] = 2;
+			i++;
+		}
+        */
+		/*ハイスコアだったら保存*/
+/*		if (time <= Maxtime) {
+			Debug.Log("最高スコア更新！");
+			PlayerPrefsX.SetFloatArray ("timeX",timeNow);
+			PlayerPrefsX.SetIntArray ("keyX",inkey);
+			PlayerPrefs.SetFloat ("Maxtime",time);
+
+			PlayerPrefs.Save();
+	
+		}*/
+
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag=="goal"){
+			if (time <= Maxtime) {
+				Debug.Log("最高スコア更新！");
+				PlayerPrefsX.SetFloatArray ("timeX",timeNow);
+				PlayerPrefsX.SetIntArray ("keyX",inkey);
+				PlayerPrefs.SetFloat ("Maxtime",time);
+
+				PlayerPrefs.Save();
+
+			}
+		}
+	}
+						
+}
